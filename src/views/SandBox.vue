5ae543d25ac765ed8ed78beb6f5163d005283642
João Vitor Guimarães <92169565+joaoemc2@users.noreply.github.com>
2024-03-19 20:33:24 -0300

    🍻 create sandbox

---
+++ src/views/SandBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sand-box"></div>
+</template>
+
+<script>
+export default {
+  name: "SandBox",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+.sand-box {
+  initial-value: 134113;
+  border-left: 13141516;
+  nav-left: auto;
+  gap: 17.474583;
+  gap: 10.485728;
+}
+</style>
